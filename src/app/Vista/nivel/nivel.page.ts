2f0d644c87723e816d116c7b92252e5f2a47d28a
JuanOrtiz2006 <juanpabloortiz2006@gmail.com>
2024-04-23 18:30:40 -0500

    /V2

--- src/app/Vista/nivel/nivel.page.ts
+++ src/app/Vista/nivel/nivel.page.ts
@@ -8,40 +8,46 @@ import { Database, object, ref, set } from '@angular/fire/database';
 })
 export class NivelPage implements OnInit {
   pagina: any;  // Variable para almacenar el parámetro 'encender'
-  led: any;
-  rout:any;
-
-  notas: any[] = ["../../../assets/Imagenes/N/Do.svg", "../../../assets/Imagenes/N/Re.svg", "../../../assets/Imagenes/N/Mi.svg",
-                    "../../../assets/Imagenes/N/Fa.svg", "../../../assets/Imagenes/N/Sol.svg", "../../../assets/Imagenes/N/La.svg", 
-                    "../../../assets/Imagenes/N/Si.svg"];
-  acordes: any[] = ["../../../assets/Imagenes/Do.svg", "../../../assets/Imagenes/Re.svg", "../../../assets/Imagenes/Mi.svg", 
-                      "../../../assets/Imagenes/Fa.svg", "../../../assets/Imagenes/Sol.svg", "../../../assets/Imagenes/La.svg", 
-                      "../../../assets/Imagenes/Si.svg", "../../../assets/Imagenes/Dom.svg", "../../../assets/Imagenes/Rem.svg", 
-                      "../../../assets/Imagenes/Mim.svg", "../../../assets/Imagenes/Fam.svg", "../../../assets/Imagenes/Solm.svg",
-                      "../../../assets/Imagenes/Lam.svg", "../../../assets/Imagenes/Sim.svg"];
-                    
+  led: any; //Variables para leer el vector de los niveles
+  led2: any;
+  rout:any; //variable para la ruta de lectura y excritura de la RTDB
+  link:string="";//Variables para las rutas de las imagenes
+  link2:string="";
+  //Vectores para las rutas de las imagenes
+  notas: any[] = ["../../../assets/Imagenes/N/Do.svg", "../../../assets/Imagenes/N/Re.svg", "../../../assets/Imagenes/N/Mi.svg", "../../../assets/Imagenes/N/Fa.svg", "../../../assets/Imagenes/N/Sol.svg", "../../../assets/Imagenes/N/La.svg", "../../../assets/Imagenes/N/Si.svg"];
+  acordes: any[] = ["../../../assets/Imagenes/Do.svg", "../../../assets/Imagenes/Re.svg", "../../../assets/Imagenes/Mi.svg", "../../../assets/Imagenes/Fa.svg", "../../../assets/Imagenes/Sol.svg", "../../../assets/Imagenes/La.svg", 
+                    "../../../assets/Imagenes/Si.svg", "../../../assets/Imagenes/Dom.svg", "../../../assets/Imagenes/Rem.svg","../../../assets/Imagenes/Mim.svg", "../../../assets/Imagenes/Fam.svg", "../../../assets/Imagenes/Solm.svg",
+                    "../../../assets/Imagenes/Lam.svg", "../../../assets/Imagenes/Sim.svg"];
   constructor(private database:Database, private router: Router, private route: ActivatedRoute) {}
   ngOnInit() {
-    const route = ref(this.database, 'Notas' );
-      object(route).subscribe(attributes => {
-        const dato = attributes.snapshot.val();
-
-      });
-    // Suscríbete a los parámetros de la ruta para obtener 'encender'
-    this.route.params.subscribe(params => {
-      this.pagina = params['encender'];  // Almacena el valor de 'encender' en la variable 'pagina'
+    this.route.params.subscribe(params => {// Suscríbete a los parámetros de la ruta para obtener 'encender'
+    this.pagina = params['encender'];  // Almacena el valor de 'encender' en la variable 'pagina'
+    this.resetValues();//Llamado del metodo resetValues
     });
   }
-  irPagina() {
-    this.router.navigate(['../inicio']);
+  irPagina(n:number) {//Metodo para navegacion de pagina
+    this.router.navigate(['../inicio']);//direccion de la pagina
   }
-
-  onSlideChange(event: any) {
-    const activeIndex = event.detail.map((item: { activeIndex: any; }) => item.activeIndex)[0];
-    this.led= activeIndex;
-    console.log('Índice del slide activo:', this.led);
-    this.rout = set(ref(this.database,'Notas') , this.led);
-    
+  onSlideChange(event: any) {//Metodo para lectura de slider
+    const activeIndex = event.detail.map((item: { activeIndex: any; }) => item.activeIndex)[0];//Obtiene el inice del slider
+    if(this.pagina==1)//Si la pagina es 1
+      {
+        this.led = activeIndex; //Le el indice del vector
+        this.rout = set(ref(this.database, 'Notas'), this.led);//Esvribe el indice en la RTDB
+        this.link = this.notas[this.led];//Coloca la ruta de la imagen
+        console.log('Índice 1:', this.led);
+      }
+    if(this.pagina==2)
+      {
+        this.led2=activeIndex
+        this.link2 = this.acordes[this.led2];
+        this.rout = set(ref(this.database, 'Notas'), this.led2+7);    
+        console.log('Índice 2:', this.led2);        
+      }
   }
-  
+  private resetValues() {
+    this.led = 0;
+    this.led2 = 0;
+    this.link = this.pagina === 1 ? this.notas[0] : this.acordes[0];
+  } 
 }
